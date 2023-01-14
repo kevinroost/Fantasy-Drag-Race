7298ce0ae0b9e8df46cce78a1fc6510fd12ebc6c
KevinRoost <kroost92@gmail.com>
2023-01-14 14:11:21 -0500

    create queen index and new queen form

--- controllers/queens.js
+++ controllers/queens.js
@@ -1,4 +1,19 @@
 import { Queen } from "../models/queen.js"
+import { Profile } from "../models/profile.js"
+
+function index(req, res) {
+  Queen.find({})
+  .then(queens => {
+    res.render('queens', {
+      queens: queens,
+      title: "All Queens"
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect("/")
+  })
+}
 
 function newQueen(req, res) {
   res.render('queens/new', {
@@ -10,6 +25,18 @@ function newQueen(req, res) {
   })
 }
 
+function create(req, res) {
+  req.body.createdBy = req.user.profile._id
+  req.body.eliminated = false
+  Queen.create(req.body)
+  .then(queens => {
+    console.log(queens);
+    res.redirect('/queens')
+  });
+}
+
 export {
   newQueen as new,
+  create,
+  index,
 }
--- routes/queens.js
+++ routes/queens.js
@@ -1,10 +1,13 @@
 import { Router } from 'express'
 import * as queensCtrl from '../controllers/queens.js'
 import { isLoggedIn } from "../middleware/middleware.js"
+import { Queen } from '../models/queen.js'
 
 const router = Router()
 
+router.get('/', queensCtrl.index)
 router.get('/new', isLoggedIn, queensCtrl.new)
+router.post('/', queensCtrl.create)
 
 export {
   router
