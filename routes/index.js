c8ddba74838d2bc7b529737f1ff4f7c7927aeda5
KevinRoost <kroost92@gmail.com>
2023-01-18 11:57:00 -0500

    revert to commit 487d12abf7259163c4f933624f21572c38afa0f3

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,25 @@
 import { Router } from 'express'
-import { tallyProfileTotalPoints, tallyQueensTotalPoints } from '../middleware/middleware.js'
 import { Profile } from "../models/profile.js"
 
 const router = Router()
 
 router.get('/', function (req, res) {
-  Profile.findById(req.user?.profile.id)
-  .populate({
-    path: 'team',
-    model: 'Queen',
-    populate: {
-      path: 'episodes',
-      model: 'Episode',
-      populate: {
-        path: 'pointEvents',
-        model: 'Event'
-      }
-    }
-  })
-  .then(profile => {
-    console.log('here', profile);
-    // tallyQueensTotalPoints(profile.team)
-    // tallyProfileTotalPoints(profile)
-    // profile.sort(function(a, b) {return b.totalPoints - a.totalPoints})
+  if (req.user) {
+    Profile.findById(req.user.profile._id)
+    .populate('team')
+    .then(profile => {
+      console.log(profile);
+      res.render('index', {
+        title: 'Home Page',
+        profile
+      })
+    })
+  } else {
     res.render('index', {
       title: 'Home Page',
       profile
     })
-  })
-  .catch(err => {
-      console.log(err)
-      res.redirect("/")
-  })
+  }
 })
 
 export {
