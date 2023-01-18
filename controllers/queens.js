387e3dbf0cc9b512c3ff4feed6ebf30cbe2d2cee
KevinRoost <kroost92@gmail.com>
2023-01-18 09:58:56 -0500

    sort profiles with index function

--- controllers/queens.js
+++ controllers/queens.js
@@ -5,31 +5,41 @@ import { tallyQueensTotalPoints } from "../middleware/middleware.js"
 
 
 function index(req, res) {
-  Queen.find({})
-  .populate({
-    path: 'episodes',
-    model: 'Episode',
-    populate: {
-      path: 'pointEvents',
-      model: 'Event'
-    }
+  Profile.find({})
+  .populate("team")
+  .then(profiles => {
+    profiles.forEach(profile => tallyQueensTotalPoints(profile.team))
+    profiles.sort(function(a, b) {return b.totalPoints - a.totalPoints})
   })
-  .then(queens => {
-    Profile.findById(req.user?.profile._id)
-    .then(profile => {
-      console.log('profile', profile);
-      tallyQueensTotalPoints(queens)
-      res.render('queens', {
-        profile,
-        queens: queens,
-        title: "All Queens",
+    Queen.find({})
+    .populate({
+      path: 'episodes',
+      model: 'Episode',
+      populate: {
+        path: 'pointEvents',
+        model: 'Event'
+      }
+    })
+    .then(queens => {
+      Profile.findById(req.user?.profile._id)
+      .then(profile => {
+        console.log('profile', profile);
+        tallyQueensTotalPoints(queens)
+        res.render('queens', {
+          profile,
+          queens: queens,
+          title: "All Queens",
+        })
+      })
+      .catch(err => {
+        console.log(err)
+        res.redirect("/")
       })
     })
     .catch(err => {
       console.log(err)
       res.redirect("/")
     })
-  })
   .catch(err => {
     console.log(err)
     res.redirect("/")
--- models/profile.js
+++ models/profile.js
@@ -1,11 +1,13 @@
 import mongoose from 'mongoose'
+import { tallyQueensTotalPoints } from '../middleware/middleware'
 
 const Schema = mongoose.Schema
 
 const profileSchema = new Schema({
   name: String,
   avatar: String,
-  team: [{ type: Schema.Types.ObjectId, ref: "Queen" }]
+  team: [{ type: Schema.Types.ObjectId, ref: "Queen" }],
+  totalPoints: Number
 }, {
   timestamps: true
 })
